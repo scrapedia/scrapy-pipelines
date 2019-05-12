2011158a6b6cdb466d200eeac492edc2d9434a76
Grammy Jiang <grammy.jiang@gmail.com>
2019-05-13 00:46:24 +1000

    Add setup.py and a tox configuration file

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+import versioneer
+
+extras_require = {}
+
+setup(
+    name='Scrapy-Pipeline',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    url='https://github.com/scrapedia/scrapy-pipelines',
+    description='A collection of scrapy item pipelines',
+    long_description=open('README.rst').read(),
+    author='Scrapedia',
+    maintainer='Scrapedia',
+    maintainer_email='scrapedia@outlook.com',
+    license='GPLv3',
+    package=find_packages(exclude=('tests', 'tests.*')),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Framework :: Scrapy',
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'LICENSE :: OSI APPROVED :: GNU GENERAL PUBLIC LICENSE V3 (GPLV3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires=[
+        'txmongo',
+        'scrapy'
+    ],
+    extras_require=extras_require,
+)
