34b6892a0047c98f73b0a8626e6342d1f78316b7
Grammy Jiang <grammy.jiang@gmail.com>
2019-06-12 13:41:07 +1000

    Add commentary for item.py

--- scrapy_pipelines/item.py
+++ scrapy_pipelines/item.py
@@ -1,5 +1,11 @@
+"""
+A customized item for MongoDB
+"""
 from scrapy.item import Field, Item
 
 
 class BSONItem(Item):
+    """
+    Pymongo creates `_id` automatcially in the object after inserting
+    """
     _id = Field()
